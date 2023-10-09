6903cebed84da450add7c3c60f86ea316af60cd9
Friedrich Szendzielorz <fszendzi@c0c1c1.42berlin.de>
2023-10-09 17:42:35 +0200

    Norminette

--- src/sort_three.c
+++ src/sort_three.c
@@ -12,44 +12,43 @@
 
 #include "../inc/push_swap.h"
 
-static t_node *find_highest(t_stack *stack)
+static t_node	*find_max(t_stack *stack)
 {
-	long highest;
-	t_node *highest_node = NULL;
-	t_node *current_node;
-	t_node *start_node; // For tracking the starting node
+	long	max;
+	t_node	*current_node;
+	t_node	*start_node;
+	t_node	*max_node;
 
 	if (stack == NULL || stack->head == NULL)
-		return NULL;
-
-	highest = LONG_MIN;
-	current_node = start_node = stack->head;
-
-	do
+		return (NULL);
+	max = LONG_MIN;
+	current_node = stack->head;
+	start_node = stack->head;
+	while (1)
 	{
-		if (current_node->data > highest)
+		if (current_node->data > max)
 		{
-			highest = current_node->data;
-			highest_node = current_node;
+			max = current_node->data;
+			max_node = current_node;
 		}
 		current_node = current_node->next;
-	} while (current_node && current_node != start_node);
-
-	return highest_node;
+		if (current_node == start_node)
+			break ;
+	}
+	return (max_node);
 }
 
-void sort_size_three(t_stack **a)
+void	sort_size_three(t_stack **a)
 {
-	t_node *highest_node;
+	t_node	*max_node;
 
-	highest_node = find_highest(*a);
-	if ((*a)->head == highest_node)
+	max_node = find_max(*a);
+	if ((*a)->head == max_node)
 	{
 		ra(a);
 		write(1, "ra\n", 3);
 	}
-		
-	else if ((*a)->head->next == highest_node)
+	else if ((*a)->head->next == max_node)
 	{
 		rra(a);
 		write(1, "rra\n", 4);
@@ -59,50 +58,4 @@ void sort_size_three(t_stack **a)
 		sa(a);
 		write(1, "sa\n", 3);
 	}
-	
-	/*
-	if (!*a || !(*a)->head || !(*a)->head->next || !(*a)->head->next->next)
-	{
-		return; // Nothing to sort
-	}
-
-	int first = (*a)->head->data;
-	int second = (*a)->head->next->data;
-	int third = (*a)->head->next->next->data;
-
-	// Case 3 1 2
-	if (first > third && second < third)
-	{
-		ra(a);
-		printf("ra\n");
-	}
-	// Case 2 1 3
-	else if (first > second && first < third)
-	{
-		sa(a);
-		printf("sa\n");
-	}
-	// Case 1 3 2
-	else if (first < third && second > third)
-	{
-		sa(a);
-		ra(a);
-		printf("sa\n");
-		printf("ra\n");
-	}
-	// Case 2 3 1
-	else if (first > third && second > first)
-	{
-		rra(a);
-		printf("rra\n");
-	}
-	// Case 3 2 1
-	else if (first > second && second > third)
-	{
-		sa(a);
-		rra(a);
-		printf("sa\n");
-		printf("rra\n");
-	}
-	*/
 }
